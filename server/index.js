e06a1b5e73a6709f3d15aba76824a2dc95184581
Andrew Juers <andrewjuers@Andrews-MBP.lan>
2025-02-10 04:21:23 -0500

    fix typos

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-const { makeid } = require('./utils');
+const { makeid } = require("./utils");
 
 app.use(cors());
 
@@ -11,64 +11,55 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "https://bricked.onrender.com", // Removed trailing slash
+        origin: "https://bricked.onrender.com",
         methods: ["GET", "POST"],
     },
 });
 
 const state = {};
-const severRooms = {};
+const serverRooms = {};
 
 io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
 
     socket.on("send_message", (data) => {
-        io.emit("receive_message", data); // Fix: Send message to all users
-        console.log(data);
+        io.emit("receive_message", data); // Broadcast message to all users
     });
 
     socket.on("newGame", handleNewGame);
     socket.on("joinGame", handleJoinGame);
 
     function handleJoinGame(roomName) {
-        const room = io.sockets.adapter.rooms[roomName];
-    
-        let allUsers;
-        if (room) {
-          allUsers = room.sockets;
+        const room = io.sockets.adapter.rooms.get(roomName); // Updated room retrieval
+
+        let numClients = room ? room.size : 0; // Get number of players in the room
+
+        if (numClients === 0) {
+            socket.emit("unknownCode");
+            return;
+        } else if (numClients > 1) {
+            socket.emit("tooManyPlayers");
+            return;
         }
-    
-        let numsevers = 0;
-        if (allUsers) {
-          numsevers = Object.keys(allUsers).length;
-        }
-    
-        if (numsevers === 0) {
-          sever.emit('unknownCode');
-          return;
-        } else if (numsevers > 1) {
-          sever.emit('tooManyPlayers');
-          return;
-        }
-    
-        severRooms[sever.id] = roomName;
-    
-        sever.join(roomName);
-        sever.number = 2;
-        sever.emit('init', 2);
-        
+
+        serverRooms[socket.id] = roomName;
+
+        socket.join(roomName);
+        socket.number = 2;
+        socket.emit("init", 2);
+
         startGameInterval(roomName);
-      }
-    
-      function handleNewGame() {
+    }
+
+    function handleNewGame() {
         let roomName = makeid(5);
-        severRooms[sever.id] = roomName;
-        sever.emit('gameCode', roomName);
-    
-        state[roomName] = initGame();
-    
-        sever.join(roomName);
-        sever.number = 1;
-        sever.emit('init', 1);
-      }
+        serverRooms[socket.id] = roomName;
+        socket.emit("gameCode", roomName);
+
+        state[roomName] = {}; // Placeholder for game state
+
+        socket.join(roomName);
+        socket.number = 1;
+        socket.emit("init", 1);
+    }
 });
